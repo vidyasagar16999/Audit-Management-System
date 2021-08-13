e2b2a5440aa707519ed2d6356e0b8a47f6864b35
Akhilesh Reddy <62151789+akhilesh137@users.noreply.github.com>
2021-08-13 17:56:48 +0530

    Delete Authorization/src directory

--- Authorization/src/main/resources/Data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (1, 'Akhilesh reddy', 'akhil', 'akhil1137', 'Project-1');
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (2, 'Vidya Sagar', 'sagar', 'sagar1234', 'Project-2');
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (3, 'Nowshad shaik', 'nowshad', 'nowshad1234', 'Project-3');
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (4, 'Jaswanth reddy', 'jaswanth', 'jaswanth1234', 'Project-4');
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (5, 'Indra Narayana reddy', 'narayana', 'narayana1234', 'Project-5');
