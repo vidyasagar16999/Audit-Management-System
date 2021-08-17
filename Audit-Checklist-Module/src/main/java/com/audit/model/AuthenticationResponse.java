9e86ee0ca7269aad88719503eeff366d91b0e587
vidyasagar16999 <49290224+vidyasagar16999@users.noreply.github.com>
2021-08-17 15:08:58 +0530

    Update AuthenticationResponse.java

--- Audit-Checklist-Module/src/main/java/com/audit/model/AuthenticationResponse.java
+++ Audit-Checklist-Module/src/main/java/com/audit/model/AuthenticationResponse.java
@@ -6,7 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 @Component
