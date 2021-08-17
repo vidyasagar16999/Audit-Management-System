c1aa20c2f4a086988b15dca3e830b98605eb9d91
vidyasagar16999 <49290224+vidyasagar16999@users.noreply.github.com>
2021-08-17 21:14:09 +0530

    Update AuditChecklistApplication.java

--- Audit-Checklist-Module/src/main/java/com/audit/AuditChecklistApplication.java
+++ Audit-Checklist-Module/src/main/java/com/audit/AuditChecklistApplication.java
@@ -3,7 +3,8 @@ package com.audit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableSwagger2
 @SpringBootApplication
 @EnableFeignClients
 public class AuditChecklistApplication {
