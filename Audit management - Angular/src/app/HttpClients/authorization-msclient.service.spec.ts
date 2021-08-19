a3488f63a964136c59fb4c78df25cbdba63343a7
vidyasagar16999 <49290224+vidyasagar16999@users.noreply.github.com>
2021-08-19 11:15:27 +0530

    Update authorization-msclient.service.spec.ts

--- Audit management - Angular/src/app/HttpClients/authorization-msclient.service.spec.ts
+++ Audit management - Angular/src/app/HttpClients/authorization-msclient.service.spec.ts
@@ -1,16 +1,50 @@
-import { TestBed } from '@angular/core/testing';
-
+import { inject, TestBed } from '@angular/core/testing';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { AuthorizationMSClientService } from './authorization-msclient.service';
+import { Microservices } from '../Models/Microservices'
 
 describe('AuthorizationMSClientService', () => {
   let service: AuthorizationMSClientService;
+  let httpMock : HttpTestingController;
+  let httpClient : HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers:[AuthorizationMSClientService]
+    });
     service = TestBed.inject(AuthorizationMSClientService);
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
   });
+
+  it("test the project manager login",() =>{
+    const AuthenticationRequest:any={
+      "username": "akhil",
+      "password": "akhil1137"
+    };
+  const dummyToken:any={
+    "token": "dummytokenstringtestvalidation"
+  };
+  service.authenticate(AuthenticationRequest).subscribe(data =>{expect(data).toEqual(dummyToken)
+  
+  });
+
+  const req = httpMock.expectOne(service._url+"auth/authenticate");
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(AuthenticationRequest);
+
+const expectedResponse = new HttpResponse({ status: 200, statusText: 'OK', body: dummyToken });
+req.event(expectedResponse)
+
+  
+});
+
+
+
 });
